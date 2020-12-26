69edd6495e1e70dd06d4d1894eee2904e4074412
crypt0box <pray.happy76@gmail.com>
2020-12-26 21:42:53 +0900

    ADD: initial commit

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,13 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 // import * as express from 'express';
-const express = require("express");
-const app = express();
+// import express = require('express');
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+app.get('/', (req, res, next) => {
+    res.send('<h1>Hello<h1>');
+});
 app.listen(3000);
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,21 @@
 // import * as express from 'express';
 // import express = require('express');
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+interface MessageRequest extends Request {
+  body: {
+    message: string;
+  }
+}
 const app = express();
+app.use(express.json());
+app.get('/', (req, res, next) => {
+  res.send('<h1>Hello<h1>');
+})
+app.post('/', (req: MessageRequest, res, next) => {
+  res.send(`<h1>I got ${req.body}<h1>`);
+})
+// エラーハンドリング
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  next();
+}) 
 app.listen(3000);
